7fbc43abaadf5f5cf37808309c58080b1b4c91dd
Prithaj Nath <prithajnath@gmail.com>
2019-11-23 01:11:00 -0500

    Port participants over to users so they can log in

--- commands.py
+++ commands.py
@@ -1,7 +1,8 @@
-from models import User
+from models import User, Participant
 from getpass import getpass
 from flask_script import Command
-from app import db
+from app import db, send_email
+from os import environ
 
 class CreateSuperUser(Command):
     def run(self):
@@ -12,8 +13,56 @@ class CreateSuperUser(Command):
         new_superuser = User(
             username=username,
             email=email,
-            password=password
+            password=password,
+            admin=True
         )
 
         new_superuser.save_to_db(db)
         print(f"superuser {username} : {email} created successfully")
+
+class CreateProfiles(Command):
+    def run(self):
+        participants = Participant.query.all()
+        users = [i.email for i in User.query.all()]
+        for participant in participants:
+            skip = input(f"Skip user {participant.email}? [y/n]\n")
+            if skip == 'y':
+                print("skipping...")
+                continue
+            elif skip == 'n':
+                password = input(f"Enter password for {participant.email}\n")
+                user = User.query.filter_by(email=participant.email).first()
+                if not user:
+                    user = User(
+                        username=f"{participant.first_name.lower()}{participant.last_name.lower()}",
+                        password=password,
+                        email=participant.email,
+                        participant=participant
+                    )
+
+                    user.save_to_db(db)
+                    print(f"Created new user {user.username}")
+                else:
+                    user.set_password(password)
+                    user.save_to_db(db)
+
+                if environ.get("ENVIRONMENT") != "test":
+                    sent = send_email(
+                        'platts.sec@gmail.com',
+                        'You got a password!!!',
+                        participant.email,
+                        "password.html",
+                        {
+                            "first_name": participant.first_name,
+                            "password": password,
+                            "url": f"{environ.get('PROTOCOL')}://{environ.get('HOSTNAME')}/profile"
+                        }
+                    )
+
+                    if sent:
+                        print(f"Created new password for user {participant.email}!!")
+                else:
+                    print(f"Created new password for user {participant.email}!!")
+
+            else:
+                break
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from passlib.hash import pbkdf2_sha256
 from mixins import dbMixin
 from flask_login import UserMixin
+from uuid import uuid1
 
 db = SQLAlchemy()
 
@@ -12,17 +13,29 @@ class User(dbMixin, UserMixin, db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    admin = db.Column(db.Boolean, unique=False, default=False)
+    participant = db.relationship("Participant", backref="user", uselist=False)
 
     def __init__(self, username="test",
                  email="test@test.io",
                  password="test",
+                 admin=False,
+                 participant=None,
                  user_type_id=""):
         self.username = username
         self.email = email
+        self.admin = admin
         self.password = pbkdf2_sha256.hash(password)
+        self.participant = participant
 
     def verify_hash(self, password, hash):
         return pbkdf2_sha256.verify(password, hash)
+
+    def set_password(self, password):
+        self.password = pbkdf2_sha256.hash(password)
+
+    def __str__(self):
+        return self.email
 
 
 class Participant(dbMixin, db.Model):
@@ -33,6 +46,7 @@ class Participant(dbMixin, db.Model):
     hint = db.Column(db.String(500))
     address = db.Column(db.String(500))
     email = db.Column(db.String(200), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True, nullable=True)
 
     def __init__(self, first_name="test",
                  last_name="test",
