a0efca3d1e79d3c5d9b10f72650cb0a29c67fa82
Prithaj Nath <prithajnath@gmail.com>
2020-10-17 03:08:36 +0530

    Stub out task runner class. Add new models and admin commands

--- commands.py
+++ commands.py
@@ -1,8 +1,11 @@
-from models import User 
+from models import User, Group, GroupsAndUsersAssociation
 from getpass import getpass
 from flask_script import Command
+from faker import Faker
 from app import db, send_email
 from os import environ
+from random import choices
+from uuid import uuid4
 
 class CreateSuperUser(Command):
     def run(self):
@@ -19,3 +22,39 @@ class CreateSuperUser(Command):
 
         new_superuser.save_to_db(db)
         print(f"superuser {username} : {email} created successfully")
+
+class SeedDatabase(Command):
+    def run(self):
+        if environ.get('ENV') != 'production':
+            fake = Faker()
+
+            users = [(fake.first_name(), fake.last_name()) for _ in range(1000)]
+            user_objects = []
+            for first_name, last_name in users:
+                username = f"{first_name}{last_name}".lower()
+                email = f"{username}_{uuid4().__str__()[:8]}@santa.io"
+                print(f"adding {username}")
+                random_user = User(
+                    username=username,
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=email,
+                    password='helloworld'       
+                )
+
+                random_user.save_to_db(db)
+                user_objects.append(random_user)
+
+            distinct = set()
+            for _ in range(50):
+                members = choices(user_objects, k=10)
+                group_name = fake.bs()
+                group = Group(name=group_name)
+                for member in members:
+                    if (member.id, group.id) not in distinct:
+                        group_assoc = GroupsAndUsersAssociation(user=member, group=group)
+                        group_assoc.save_to_db(db)
+                        distinct.add((member.id, group.id))
+            
+        else:
+            print("Can't run this command in production")
