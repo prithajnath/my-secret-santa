8da627874b201e1de7ca8f40dc65f41e16af27af
Prithaj Nath <prithajnath@gmail.com>
2020-12-02 12:47:20 +0530

    Add more gunicorn workers and increased timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 python manage.py db migrate
 python manage.py db upgrade
-gunicorn --bind 0.0.0.0:$PORT app:app
+gunicorn -w $(expr 2 \* $(nproc) + 1) \
+    --timeout 120 \
+    --bind 0.0.0.0:$PORT \
+    app:app
 
