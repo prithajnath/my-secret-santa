3bf19927c40f454b71902bda91b5365b9e4e9f48
Prithaj Nath <prithajnath@gmail.com>
2020-12-02 11:43:42 +0530

    Blow up bash errors

--- jobs/build_container_image.sh
+++ jobs/build_container_image.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 docker-compose -f docker-compose-prod.yml build
 docker-compose -f docker-compose-prod.yml push
--- jobs/deploy_all_services.sh
+++ jobs/deploy_all_services.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 docker-compose -f docker-compose-prod.yml pull
 docker-compose -f docker-compose-prod.yml down
 docker-compose -f docker-compose-prod.yml up -d
