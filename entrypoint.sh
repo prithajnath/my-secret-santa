313f5d3723cf2d9e89545e8124fd61676c49b25c
Prithaj Nath <prithajnath@gmail.com>
2020-12-02 13:06:11 +0530

    Set log level to DEBUG

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ python manage.py db migrate
 python manage.py db upgrade
 gunicorn -w $(expr 2 \* $(nproc) + 1) \
     --timeout 120 \
+    --log-level=debug \
     --bind 0.0.0.0:$PORT \
     app:app
 
