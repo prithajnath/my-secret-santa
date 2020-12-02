4fdfd96201df98acd92cdb7685694eb4192f021d
Prithaj Nath <prithajnath@gmail.com>
2020-12-02 13:27:04 +0530

    Increase gunicorn concurrency (threads)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 python manage.py db migrate
 python manage.py db upgrade
-gunicorn -w $(expr 2 \* $(nproc) + 1) \
+
+CONCURRENCY=$(expr 2 \* $(nproc) + 1)
+gunicorn -w  $CONCURRENCY \
     --timeout 120 \
     --log-level=debug \
+    --threads=$CONCURRENCY \
     --bind 0.0.0.0:$PORT \
     app:app
 
