d2805cc6f67d847380cae3dccc00d92638365d8a
Prithaj Nath <prithajnath@gmail.com>
2020-12-07 06:55:01 -0500

    Test suite needs different build context and network isolation (#12)

--- test.sh
+++ test.sh
@@ -14,22 +14,22 @@ santatext
 
 docker-compose up -d
 
-while [ $(docker ps | grep secret-santa | wc -l ) -ne "4" ]; do
+while [ $(docker ps | grep dev-secret-santa | wc -l ) -ne "4" ]; do
     sleep 5
 done
 
-container_id=$(docker ps | grep web_1 | awk '{ print $1 }')
+container_name=dev-secret-santa-web
 
 # Check if database has been initialized yet (Relations MUST exist)
 echo "$(date) : checking database readiness"
 while true; do
-    db=$(docker exec $container_id psql -h secret-santa-postgres -U postgres --db postgres -c "\\d")
+db=$(docker exec $container_name psql -h secret-santa-postgres -U postgres --db postgres -c "\\d")
     [ $? -eq 0 ] && [[ $db =~ "users" ]] && echo $db && break
     sleep 5
 done
 
 echo "$(date) : running the secret santa test suite"
-docker exec $container_id pytest -vv
+docker exec $container_name pytest -vv
 
 echo "$(date) cleaning up"
 docker-compose rm -vfs
