f9487de79d413f5f1646affef31d4202798845e3
Prithaj Nath <prithajnath@gmail.com>
2020-12-08 11:07:50 +0530

    Check disk space available before building puppeteer image

--- test.sh
+++ test.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+clean_up () {
+    ARG=$?
+    echo "$(date) cleaning up"
+    docker image rm -f santa-puppet
+    docker-compose rm -vfs
+    exit $ARG
+}
+trap clean_up EXIT
+
+
 echo "$(date) : spawning the secret santa container army"
 cat << santatext
 
@@ -31,11 +41,9 @@ done
 echo "$(date) : running the secret santa test suite"
 docker exec $container_name pytest -vv
 
-# Build the Puppeteer image
-docker build -t santa-puppet -f puppeteer.Dockerfile .
-
 # Insert user helenkeller into the db
 pass=\$pbkdf2-sha256\$29000\$xFiLUeq9N2YMgVBKqdXaOw\$kcnjB2FPGFk/cr0Yhyw7T4bcCWNaW/NqF8xa1Ktf1OA
+
 read -r -d '' SQL << sql
 INSERT INTO
   USERS(
@@ -53,7 +61,23 @@ echo "$SQL"
 
 docker exec $container_name psql -h secret-santa-postgres -U postgres --db postgres -c "$SQL"
 
-docker run --security-opt seccomp=chrome.json --network=my-secret-santa_santa_test_network santa-puppet
+# Check disk space available before building the puppeteer image. Blow up if low on disk space
+read -r -d '' PYTHON << py
+import shutil
+import sys
 
-echo "$(date) cleaning up"
-docker-compose rm -vfs
+PUPPETEER_IMAGE_SIZE=2.3
+
+_, _, free = shutil.disk_usage("/")
+enough_disk_space = free / (2**30)
+print(f"{enough_disk_space:.2f} GB disk space available")
+if enough_disk_space - PUPPETEER_IMAGE_SIZE < 1:
+    sys.exit(1)
+py
+
+python3 -c "$PYTHON"
+
+# Build the Puppeteer image
+docker build -t santa-puppet -f puppeteer.Dockerfile .
+
+docker run --security-opt seccomp=chrome.json --network=my-secret-santa_santa_test_network santa-puppet
