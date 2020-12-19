f06f67a8c2e582ddd45e72f57a17b24224453a2b
Prithaj Nath <prithajnath@gmail.com>
2020-12-19 23:04:25 +0530

    legacy pruning (many more to come)

--- sql/sql.py
+++
@@ -1,7 +0,0 @@
-import os
-
-sql_dir = os.path.join(os.getenv("APP_ROOT"), "sql")
-queries = {}
-for sql_file in os.listdir(sql_dir):
-    with open(os.path.join(sql_dir, sql_file)) as f:
-        queries[sql_file] = "".join(f.readlines())
