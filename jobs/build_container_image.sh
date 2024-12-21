b8af996f091cc04292f6d93b77e97fec58ec0ee1
Prithaj Nath <prithajnath@gmail.com>
2024-12-20 23:17:22 -0500

    s/docker-compose/docker compose/g

--- jobs/build_container_image.sh
+++ jobs/build_container_image.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
-docker-compose -f docker-compose-prod.yml build
-docker-compose -f docker-compose-prod.yml push
+docker compose -f docker-compose-prod.yml build
+docker compose -f docker-compose-prod.yml push
